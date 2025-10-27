60c47374aeea7437594286df4333721e0cdbae0c
DrMack97 <delgadoantolinezdavidalejandro@gmail.com>
2025-10-27 14:56:26 +0100

    ejercicio listas

---
+++ A1/Listas/Listas1.py
@@ -0,0 +1,17 @@
+squares = [1, 4, 9, 16, 25] 
+print(squares)
+
+nombres = ['Ana', 'Luis', 'Carlos', 'Marta']
+
+print(nombres[0])  # Acceder al primer elemento
+print(nombres[3])  # Acceder al ultimo elemento
+
+nombres[1] = 'maria'  # Cambiar el segundo elemento
+nombres[2] = 'lucia'  # Cambiar el tercer elemento
+print(nombres)
+
+long = len(squares)
+print("La longitud de la lista squares es:", long)
+
+long_nombres = len(nombres)
+print("La longitud de la lista nombres es:", long_nombres)
---
+++ A1/Listas/Numeros.py
@@ -0,0 +1,20 @@
+#contar aparaciones 
+
+list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
+
+apariciones = list_num.count(5)
+print("El numero 5 aparece", apariciones, "veces en la lista.")
+
+# suma de elementos
+list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
+
+suma = sum(list_num)
+print("La suma de los elementos de la lista es:", suma)
+
+# encontrar el valor maximo y minimo
+list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
+
+maximo = max(list_num)
+minimo = min(list_num)
+print("El valor maximo en la lista es:", maximo)
+print("El valor minimo en la lista es:", minimo)

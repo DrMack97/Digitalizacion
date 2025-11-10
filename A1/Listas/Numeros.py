7efc6fbef945a7894263665116acdc3358c49f46
DrMack97 <delgadoantolinezdavidalejandro@gmail.com>
2025-11-10 14:53:44 +0100

    duplicado

--- A1/Listas/Numeros.py
+++ A1/Listas/Numeros.py
@@ -18,3 +18,32 @@ maximo = max(list_num)
 minimo = min(list_num)
 print("El valor maximo en la lista es:", maximo)
 print("El valor minimo en la lista es:", minimo)
+
+# invertir la lista
+list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
+list_num.reverse()
+print("La lista invertida es:", list_num)
+
+# concatenar dos listas
+list_num1 = [2, 7, 4, 5, 1]
+list_num2 = [8, 3, 9, 6, 10]
+lista_concatenada = list_num1 + list_num2
+print("La lista concatenada es:", lista_concatenada)
+
+# verificar duplicados en la lista y contar duplicados
+list_num3 = [6,9,6,9]
+print("Lista original:", list_num3)
+duplicados = set()
+for num in list_num3:
+    if list_num3.count(num) > 1:
+        duplicados.add(num)
+print("Numeros duplicados en la lista:", duplicados)
+for num in duplicados:
+    print(f"El numero {num} aparece {list_num3.count(num)} veces.")
+    
+
+
+
+
+
+        
